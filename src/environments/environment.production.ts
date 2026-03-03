3e0417ecbb3cfd38c25d31d97dc038250b7a9741
rodriguezjuacam <rodriguezjuacam@globalhitss.com>
2026-03-02 21:25:13 -0500

    faet(file): update url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://api.beccafact.com/api/v1',
-  apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'https://beccafact-backend.onrender.com',
 };
